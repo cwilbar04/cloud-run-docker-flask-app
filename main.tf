90e6a146738745ac584cc7286b00461afb8b880f
Christopher Wilbar <cwilbar@alumni.nd.edu>
2021-05-20 20:49:08 -0500

    add terraform wrapper to backend. test docker pub

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 # Configure Terraform Cloud Backend
-backend "remote" {
-  # The name of your Terraform Cloud organization.
-  organization = "CWILBAR"
+terraform{
+    backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "CWILBAR"
 
-  # The name of the Terraform Cloud workspace to store Terraform state files in.
-  workspaces {
-    name = "cloud-run-docker-flask-app"
-  }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+        name = "cloud-run-docker-flask-app"
+    }
+    }s
 }
