d125f24cd07ef988d52515ef5036ac317e70d9aa
Christopher Wilbar <cwilbar@alumni.nd.edu>
2021-06-04 00:35:11 -0500

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,19 +1,7 @@
 # Configure Terraform Cloud Backend
-# terraform {
-#   backend "remote" {
-#     # The name of your Terraform Cloud organization.
-#     organization = "CWILBAR"
-
-#     # The name of the Terraform Cloud workspace to store Terraform state files in.
-#     workspaces {
-#       prefix = "cloud-run-docker-flask-app-"
-#     }
-#   }
-# }
-
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "CWILBAR"
 
     workspaces {
